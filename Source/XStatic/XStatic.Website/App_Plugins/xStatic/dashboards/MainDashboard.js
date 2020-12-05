e3f0f608a2b692b283e80969f12f83919cd3babf
Sam Mullins <sammullins.co.uk@gmail.com>
2020-12-05 17:46:37 +0000

    Created a dashboard for pressing the buttons.

---
+++ Source/XStatic/XStatic.Website/App_Plugins/xStatic/dashboards/MainDashboard.js
@@ -0,0 +1,28 @@
+﻿angular.module("umbraco")
+    .factory("xStaticResource", function ($http, umbRequestHelper) {
+        return {
+            getAll: function (type, sortColumn, sortOrder) {
+                if (sortColumn == undefined)
+                    sortColumn = "";
+                if (sortOrder == undefined)
+                    sortOrder = "";
+                return umbRequestHelper.resourcePromise(
+                    $http.get("/umbraco/backoffice/UIOMatic/GeneratedSites/GetNodes?id=generatedSite&application=xstatic&tree=&use=main&culture="),
+                    'Failed to get all'
+                );
+            },
+        }
+    })
+    .controller("xStaticMainDashboardController", function ($scope, xStaticResource) {
+        var vm = this;
+
+        vm.sites = [];
+
+        xStaticResource.getAll().then(function (data) {
+            vm.sites = data;
+
+            console.log(vm.menu);
+        });
+
+
+});
