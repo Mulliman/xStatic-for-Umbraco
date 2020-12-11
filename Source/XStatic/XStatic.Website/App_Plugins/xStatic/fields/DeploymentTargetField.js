015e151285d9658317c6ef900200003bad860be3
Sam Mullins <sammullins.co.uk@gmail.com>
2020-12-11 08:45:54 +0000

    Open netlify in new tab.

--- Source/XStatic/XStatic.Website/App_Plugins/xStatic/fields/DeploymentTargetField.js
+++ Source/XStatic/XStatic.Website/App_Plugins/xStatic/fields/DeploymentTargetField.js
@@ -3,7 +3,7 @@
         $scope.deploymentTypes = [{
             id: "netlify",
             name: "Netlify",
-            help: "First create a personal access token at <a href='https://app.netlify.com/user/applications'>https://app.netlify.com/user/applications</a>. <br/> Then go to the site settings for your site, and in Site details/Site information you should see an API ID.",
+            help: "First create a personal access token at <a target='_blank' href='https://app.netlify.com/user/applications'>https://app.netlify.com/user/applications</a>. <br/> Then go to the site settings for your site, and in Site details/Site information you should see an API ID.",
             fields: {
                 PersonalAccessToken: "",
                 SiteId: ""
