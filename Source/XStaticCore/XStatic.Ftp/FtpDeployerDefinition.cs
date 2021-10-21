51b89ccc41ac85a491fe0c77478ecfd8001ca02f
Sam Mullins <sammullins.co.uk@gmail.com>
2021-10-21 21:53:08 +0100

    Added FTP deployer.

---
+++ Source/XStaticCore/XStatic.Ftp/FtpDeployerDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using XStatic.Core.Deploy;
+
+namespace XStatic.Ftp
+{
+    public class FtpDeployerDefinition : IDeployerDefinition
+    {
+        public string Id => FtpDeployer.DeployerKey;
+
+        public string Name => "FTP";
+
+        public string Help => "The remote FTP files will be mirrored to match the generated site.";
+
+        public IEnumerable<string> Fields => new[]
+        {
+            "Hostname",
+            "Username",
+            "Password",
+            "Folder",
+            "Port"
+        };
+    }
+}
---
+++ Source/XStaticCore/XStatic.Ftp/FtpExtensions.cs
@@ -0,0 +1,14 @@
+﻿using XStatic.Core.App;
+
+namespace XStatic.Ftp
+{
+    public static class FtpExtensions
+    {
+        public static IDeployServiceBuilder AddGitDeployer(this IDeployServiceBuilder builder)
+        {
+            builder.AddDeployer(new FtpDeployerDefinition(), (x) => new FtpDeployer(x));
+
+            return builder;
+        }
+    }
+}
